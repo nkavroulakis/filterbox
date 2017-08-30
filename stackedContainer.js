8d6bec13a5f7a726142b2257abf4a13e5920f4a1
nkavroulakis <nikolaos.kavroulakis@hotmail.com>
2017-08-30 07:35:09 +0100

    Add files via upload

---
+++ stackedContainer.js
@@ -0,0 +1,43 @@
+define(["jquery", "qvangular", "qlik"], function($, qva, qlik) {
+
+    qva.directive("stackedContainer", function() {
+        return {
+ //           scope: 0,
+            priority: 0,
+            restrict: 'A',
+            link: function($scope, $element) {
+                // execute if the width of the container changes
+                //debugger;
+                $scope.$watch('stackedMode', function (newValue) {
+                    stackContainer($element, $scope);
+                })
+            }
+        }
+    })
+
+
+    /** stackContainer()
+    * If the space needed by the tabs is bigger than the container's
+    * then go into stacked mode.
+    * @param {Object} $element
+    */
+    function stackContainer($element, $scope){
+        var container_width = $element.find(".tab_container").width();
+        var tabs_width = 25;
+
+        $element.parent().removeClass("stacked");
+        $element.find(".tabs").children().each(function(index) {
+            tabs_width += $(this).outerWidth();
+        });
+
+        if (container_width < tabs_width && !$element.parent().hasClass("stacked") || $scope.stackedMode) {
+            //debugger;
+            $element.parent().addClass("stacked");
+            var outerHeight = 0;
+            outerHeight = $element.find("h3.tab_drawer_heading:first").outerHeight() * $element.find("h3.tab_drawer_heading").length;
+            outerHeight += 40;  // padding for the item
+
+            $element.find("div.tab_content:visible").height($element.find(".tab_container").height() - outerHeight);
+        }
+    }
+});
